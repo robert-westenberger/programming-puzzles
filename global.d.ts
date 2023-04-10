ef40a3fa27a679bfc532cc5205fd4a7b3e3f9725
Robert Westenberger <robert.westenberger@healthtrio.com>
2023-04-10 09:46:28 -0700

    map

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,8 @@
 declare module '@datastructures-js/priority-queue';
+
+
+declare interface Array<T> {
+  myMap<U>(callbackfn: (
+    value: T,
+    index: number, array: T[]) => U, thisArg?: any): U[];
+}
